4822776170f9aeb2b78b2911a17d7149d6b5d3f6
SwitchctiwS <l.t_jared_thibault@hotmail.com>
2017-02-04 19:02:08 -0500

    read up to ch03

--- ch02/sec07/main.c
+++ ch02/sec07/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
+#include <math.h>
+//#include <ctype.h>
+//#include <limits.h>
+
+int htoi(char* s);
 
 int main(void) {
+	char s[] = {'0', 'x', 'F', 'F', 'F', 'F', 'F', '\0'};
 
+	printf("%d", htoi(s));
+	return 0;
 }
 
 /* htoi: converts hex to dec (int)
@@ -11,5 +19,39 @@ Allowable digits are 0..9, a..f, A..F
 Params:
 	s -> hex number to convert */
 int htoi(char* s) {
+	int str_len = strlen(s);
+	int result = 0;
+	int i, j;
 
+	// Remove "0x" or "0X" from the number
+	if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) {
+		for (i = 0; i < str_len - 1; i++) {
+			s[i] = s[i + 2];
+		} 
+	}
+
+	str_len = strlen(s);
+
+	i = 0;
+	while (i < str_len) {
+		// Convert letters to numbers
+		if (s[i] == 'a' || s[i] == 'A') {
+			result += 10 * pow(16, i);
+		} else if (s[i] == 'b' || s[i] == 'B') {
+			result += 11 * pow(16, i);
+		} else if (s[i] == 'c' || s[i] == 'C') {
+			result += 12 * pow(16, i);
+		} else if (s[i] == 'd' || s[i] == 'D') {
+			result += 13 * pow(16, i);
+		} else if (s[i] == 'e' || s[i] == 'E') {
+			result += 14 * pow(16, i);
+		} else if (s[i] == 'f' || s[i] == 'F') {
+			result += 15 * pow(16, i);
+		} else {
+			result += (s[str_len - i - 1] - '0') * pow(16, i);
+		}
+
+		i++;
+	}
+	return result;
 }
---
+++ ch02/sec09/setbits/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+}
+
+/* setbits
+Params:
+	x -> initial bits
+	n -> number of bits
+	p -> starting position
+	y -> bits to change x with */
+int setbits(int x, int p, int n, int y) {
+	
+
+	return x;
+}
